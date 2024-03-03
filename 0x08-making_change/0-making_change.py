4504e282fbf7bbeee8f92efd24cdaccaed320522
ambrose <kolus7381@gmail.com>
2024-03-03 17:13:49 +0100

    making changes to coins

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Change comes from within
+"""
+
+
+def makeChange(coins, total):
+    """Given a pile of coins of different values,
+    determine the fewest number of coins needed to
+    meet a given amount total
+    """
+
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
